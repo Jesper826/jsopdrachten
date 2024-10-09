ead3b3cdc48aa671f9d7d3f3d8bc3e18ee6c2a0d
JespervanNiekerk <jespervanniekerk1@gmail.com>
2024-10-09 12:03:08 +0200

    Add files via upload

---
+++ tijdreizen.js
@@ -0,0 +1,11 @@
+const date3 = newdate(1985,7,12);
+console.log(date3);
+
+const date4 = newdate(1955,9,23);
+console.log(date4); 
+
+const date5 = newdate(2015,8,31);
+console.log(date5);
+
+const date6 = newdate(1885,1,4);
+console.log(date6);
